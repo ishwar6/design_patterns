a7d9121fac0e9cadffb8a8c9a922b0867ac16779
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2023-12-27 22:33:57 +0530

    intro

---
+++ architecture/cqrs.py
@@ -0,0 +1,4 @@
+#https://learn.microsoft.com/en-us/azure/architecture/patterns/cqrs
+#CQRS stands for Command and Query Responsibility Segregation, a pattern that separates read and update operations for a data store. 
+#Implementing CQRS in your application can maximize its performance, scalability, and security. 
+#The flexibility created by migrating to CQRS allows a system to better evolve over time and prevents update commands from causing merge conflicts at the domain level.
