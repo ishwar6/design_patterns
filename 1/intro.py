03fb63fb796a1d09f9a747d8ab621b1d6b386629
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2025-01-14 15:11:58 +0530

    Singleton pattern for logger added

--- 1/intro.py
+++ 1/intro.py
@@ -28,3 +28,47 @@ logger2.close()
 # Multiple instances lead to scattered log handling.
 # If switching to a cloud-based logger, changes are required in multiple places.
 
+##############################################################################################################################################################################
+#################################################################### Logger with Singleton Pattern ##########################################################################
+# With Singleton:
+# A single logger instance ensures centralized log management.
+
+class SingletonLogger:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(SingletonLogger, cls).__new__(cls, *args, **kwargs)
+            cls._instance.file = open("logfile.txt", "a")
+        return cls._instance
+
+    def write_log(self, message):
+        self.file.write(message + "\n")
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
+
+# Single logger instance
+logger1 = SingletonLogger()
+logger1.write_log("Transaction started.")
+
+logger2 = SingletonLogger()
+logger2.write_log("Transaction completed.")
+
+# if we have to use logger across multiple files
+# Both loggers are the same instance
+logger1.close()
+
+
+##############################################################################################################################################################################
+#################################################################### Logger with Singleton Pattern ##########################################################################
+
+# Benefits:
+# Centralized logging ensures consistency.
+# Changing the logging backend (e.g., file to cloud) requires minimal code modification.
+
+
+
+
+
