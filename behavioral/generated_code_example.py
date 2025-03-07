d31bff8ef4a3dd4fa5c2da891dcd823d8a6adecd
Rahuldevpy96 <iisudrj11@gmail.com>
2025-03-07 14:11:54 +0530

    Implement user behavior logging and history management features.

--- behavioral/generated_code_example.py
+++ behavioral/generated_code_example.py
@@ -1,29 +1,19 @@
 python
-class BehavioralFeature:
-    def __init__(self, user_data):
-        self.user_data = user_data
+class UserBehavior:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.action_log = []
 
-    def analyze_behavior(self):
-        return {
-            "average_session_time": self.calculate_average_session_time(),
-            "most_active_time": self.calculate_most_active_time(),
-            "engagement_score": self.calculate_engagement_score(),
-        }
+    def log_action(self, action):
+        self.action_log.append(action)
 
-    def calculate_average_session_time(self):
-        total_time = sum(session['duration'] for session in self.user_data['sessions'])
-        return total_time / len(self.user_data['sessions'])
+    def get_action_history(self):
+        return self.action_log
 
-    def calculate_most_active_time(self):
-        time_count = {}
-        for session in self.user_data['sessions']:
-            hour = session['start_time'].hour
-            if hour in time_count:
-                time_count[hour] += 1
-            else:
-                time_count[hour] = 1
-        return max(time_count, key=time_count.get)
+    def clear_history(self):
+        self.action_log.clear()
 
-    def calculate_engagement_score(self):
-        return (len(self.user_data['interactions']) / 
-                len(self.user_data['sessions'])) * 100
+    def most_common_action(self):
+        if not self.action_log:
+            return None
+        return max(set(self.action_log), key=self.action_log.count)
