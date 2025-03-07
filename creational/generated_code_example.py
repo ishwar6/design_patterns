8ad5997652d60df6056399501924db7a8fb71127
Rahuldevpy96 <iisudrj11@gmail.com>
2025-03-07 13:01:36 +0530

    Implement singleton pattern with SingletonMeta for consistent instance management.

---
+++ creational/generated_code_example.py
@@ -0,0 +1,19 @@
+python
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Singleton(metaclass=SingletonMeta):
+    def __init__(self):
+        self.value = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
